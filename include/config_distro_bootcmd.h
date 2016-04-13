01ac146d1e2ea66f3d1a53dfbe8885e59e4114ce
Guillaume GARDET <guillaume.gardet@free.fr>
2016-04-13 13:44:29 +0200

    XXX openSUSE XXX: Prepend partition 3 (and 4 for chromebook snow) to the list of boot partition to load DTB before EFI. It used to be 2 and 3 up to python-kiwi 9.18.34, since swap was the last partition at that time. Keep #2 for compatibility purpose. Fix boo#1163723 Also add new folders to find DTB

--- include/config_distro_bootcmd.h
+++ include/config_distro_bootcmd.h
@@ -166,7 +166,7 @@
 		"load ${devtype} ${devnum}:${distro_bootpart} "           \
 			"${fdt_addr_r} ${prefix}${efi_fdtfile}\0"         \
 	\
-	"efi_dtb_prefixes=/ /dtb/ /dtb/current/\0"                        \
+	"efi_dtb_prefixes=/ /dtb/ /dtb/current/ /boot/ /boot/dtb/ /boot/dtb/current/\0" \
 	"scan_dev_for_efi="                                               \
 		"setenv efi_fdtfile ${fdtfile}; "                         \
 		BOOTENV_EFI_SET_FDTFILE_FALLBACK                          \
@@ -545,7 +545,7 @@
 	"scan_dev_for_boot_part="                                         \
 		SCAN_DEV_FOR_BOOT_PARTS                                   \
 		"env exists devplist || setenv devplist 1; "              \
-		"for distro_bootpart in ${devplist}; do "                 \
+		"for distro_bootpart in 3 4 2 ${devplist}; do "             \
 			"if fstype ${devtype} "                           \
 					"${devnum}:${distro_bootpart} "   \
 					"bootfstype; then "               \
