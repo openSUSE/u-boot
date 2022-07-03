287e122416beb427b6b5a1677d3b6312fe25a0c2
Michal Suchanek <msuchanek@suse.de>
2022-07-03 18:25:39 +0200

    cmd: boot: add brom cmd to reboot to FEL mode
    p-boot uses RTC GPR 1 value 0xb0010fe1 to flag FEL boot on A64
    
    Default to the same.
    
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>

--- arch/arm/include/asm/arch-sunxi/cpu.h
+++ arch/arm/include/asm/arch-sunxi/cpu.h
@@ -22,4 +22,15 @@
 #define SOCID_H5	0x1718
 #define SOCID_R40	0x1701
 
+#if defined(CONFIG_SUNXI_RTC_FEL_ENTRY_GPR) && (CONFIG_SUNXI_RTC_FEL_ENTRY_GPR >= 0)
+#ifdef CONFIG_MACH_SUN8I_H3
+#define SUNXI_FEL_ENTRY_ADDRESS 0xffff0020
+#define SUNXI_RTC_GPR_OFFSET 0x100
+#define SUNXI_FEL_REG  (SUNXI_RTC_BASE + SUNXI_RTC_GPR_OFFSET + CONFIG_SUNXI_RTC_FEL_ENTRY_GPR * 4)
+#endif
+#endif
+#ifndef __ASSEMBLY__
+void set_rtc_fel_flag(void);
+#endif
+
 #endif /* _SUNXI_CPU_H */
