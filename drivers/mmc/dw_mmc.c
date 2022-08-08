openSUSE/u-boot

commit c077c057a49d47cdc029cce654e9895ea987fce5

mmc: dw_mmc: fix timeout calculate method
There are two cases not been considered:
- use uint for timeout, it will overflow when size bigger than 512KB for
  it *8*1000 at the beginning, but we may use size up to 32MB; The
  'timeout' will overflow if size bigger than 51.2MB after this fix, which
  should be enough for U-Boot;
- The timeout is using clock speed for data rate, but the device may not
  have such high speed, eg. clock is 52MHz while the device write speed may
  be less than 10MB/s, and we may use up to 150MHz clock.

Fix them in this patch, the max timeout is about 6500 when size is 32MB
after fix.

Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

drivers/mmc/dw_mmc.c
@@ -119,11 +119,12 @@ static unsigned int dwmci_get_timeout(struct mmc *mmc, const unsigned int size)
 {
 	unsigned int timeout;
 
-	timeout = size * 8 * 1000;	/* counting in bits and msec */
-	timeout *= 2;			/* wait twice as long */
+	timeout = size * 8;	/* counting in bits */
+	timeout *= 10;		/* wait 10 times as long */
 	timeout /= mmc->clock;
 	timeout /= mmc->bus_width;
 	timeout /= mmc->ddr_mode ? 2 : 1;
+	timeout *= 1000;	/* counting in msec */
 	timeout = (timeout < 1000) ? 1000 : timeout;
 
 	return timeout;

commit 8353b6e076d362e56e6e66b636230186ff41767b

mmc: dw_mmc: Increase timeout
The timeout is insufficient for reading from SD card on rk3399.

While at it change the timeout type to match the timer.

Fixes: c077c057a4 ("mmc: dw_mmc: fix timeout calculate method")
Signed-off-by: Michal Suchanek <msuchanek@suse.de>

drivers/mmc/dw_mmc.c
@@ -119,12 +119,12 @@ static int dwmci_fifo_ready(struct dwmci_host *host, u32 bit, u32 *len)
 	return 0;
 }
 
-static unsigned int dwmci_get_timeout(struct mmc *mmc, const unsigned int size)
+static unsigned long dwmci_get_timeout(struct mmc *mmc, const unsigned int size)
 {
-	unsigned int timeout;
+	unsigned long timeout;
 
 	timeout = size * 8;	/* counting in bits */
-	timeout *= 10;		/* wait 10 times as long */
+	timeout *= 50;		/* wait 50 times as long */
 	timeout /= mmc->clock;
 	timeout /= mmc->bus_width;
 	timeout /= mmc->ddr_mode ? 2 : 1;
@@ -138,9 +138,9 @@ static int dwmci_data_transfer(struct dwmci_host *host, struct mmc_data *data)
 {
 	struct mmc *mmc = host->mmc;
 	int ret = 0;
-	u32 timeout, mask, size, i, len = 0;
+	u32 mask, size, i, len = 0;
 	u32 *buf = NULL;
-	ulong start = get_timer(0);
+	ulong timeout, start = get_timer(0);
 	u32 fifo_depth = (((host->fifoth_val & RX_WMARK_MASK) >>
 			    RX_WMARK_SHIFT) + 1) * 2;
 